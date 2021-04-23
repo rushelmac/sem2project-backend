4b71f2512ebf760dd7a675a80cbee9e81ab5d5b0
cognitive-ninja <saurabhhirugade@gmail.com>
2021-04-23 15:56:31 +0530

    updated

--- app.js
+++ app.js
@@ -1,41 +1,60 @@
-var createError = require('http-errors');
+// var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
-var app = express();
+const app = express();
+
+if(!process.env.WC_jwtPrivateKey){
+    console.error('FATAL ERROR: jwtPrivateKey is not defined');
+    process.exit(1);
+}
+
+//databases connection
+const uri = "mongodb+srv://TeamBakchodi:HumBakchodiKarenge@cluster0.jg9l3.mongodb.net/WC-Storage?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+.then(() => console.log('Connected to MongoDB...'))
+.catch(err => console.error('Failed to connect...'));
+
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/auth', authRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
-app.use(function(err, req, res, next) {
+// app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
-module.exports = app;
+const port = process.env.PORT || 5000;
+app.listen(port, ()=> {
+    console.log(`Server is running on ${port}`);
+});
