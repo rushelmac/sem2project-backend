6e600000eaa19cd74f8db8b476de5e436218294b
rushelmac <rushishelke633@gmail.com>
2021-05-27 19:39:37 +0530

    Created feeds routing module

---
+++ routes/feed.js
@@ -0,0 +1,8 @@
+const express   = require("express"),
+    router      = express.Router();
+
+// POST route. Create feed
+// GET route. fetch latest feeds. (Take argument from the params, filter feeds by target audience, sort by timestamp)
+// GET route. Fetch the object (Specific by ID/author)
+// UPDATE route. Update the feed (Only by author)
+// DELETE route. Delete the feed (By author)
