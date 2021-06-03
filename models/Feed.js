5da7f5f05be97a782ef24af5fca005dcbc4a07d9
rushelmac <rushishelke633@gmail.com>
2021-06-03 22:13:46 +0530

    Updated feeds model

--- models/Feed.js
+++ models/Feed.js
@@ -1,11 +1,24 @@
 const mongoose = require("mongoose");
 
 const feedSchema = mongoose.Schema({
-    author  : mongoose.Schema.Types.ObjectId,
-    text    : String,
+    author  : {
+        type    : mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    text    : {
+        type: String,
+        required: true
+    },
+    picture : {
+        type: String
+    },
     target_audience : String,
     target_branch   : String,
-    picture : String
+    comments: [{
+        author  : mongoose.Schema.Types.ObjectId,
+        text    : String,
+        
+    }]
 },{timestamps: true});
 
 module.exports.Feed = mongoose.model("Feed", feedSchema); 
