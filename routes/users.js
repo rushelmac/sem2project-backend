d00f3b8856e532215d502c6675764d6cb1d0b843
rushelmac <rushishelke633@gmail.com>
2021-06-07 14:53:26 +0530

    Moved register and email confimation routes to auth to avoid middleware

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,8 @@
 //REGISTER ENDPOINT
 const express   = require('express'),
-    bcrypt      = require('bcrypt'),
     _           = require('lodash'),
     router      = express.Router(),
     jwt         = require('jsonwebtoken'),
-    sendMail    = require("../controllers/verify-mail"),
     { User, validateUser, validateParams } = require('../models/User');
 
 // Allowing the application to read jwt's in the form of json
@@ -25,76 +23,18 @@ router.get("/getid", async (req, res) => {
         res.status(400).send('Invalid Token.')
     }
 });
-
+// #### Tested: works fine
 // Route to get all users: Tested, Works fine
 router.get('/', async (req, res) => {
-    const users = await User.find().sort('first_name');
+    const users = await User.find().select("info").sort('info.first_name');
     res.send(users);
 });
 
 // Route to get a specific user by id. Tested, Works fine
 router.get('/current', async (req, res) => {
-    const { error } = validateParams(req.params);
-    if(error) return res.status(400).send(error.details[0].message);
-
-
-    const found = await User.findById(req.user._id);
+    const found = await User.findById(req.user._id).select("info");
     if(!found) return res.status(404).send('User not found with given ID');
     res.send(found);
-});
-
-// User register route.
-router.post('/' , async (req, res ) =>{
-    // Check for valid user schema.
-    const { error } = validateUser(req.body);
-    if(error) return res.status(400).send(error.details[0].message); 
-    
-    // Check for existing email.
-    let newUser = await User.findOne( { email: req.body.email });
-    if(newUser) return res.status(400).send("User already registered with this Email-ID");
-
-    // Create user object with hashed password and store it to databaseō.
-    newUser = new User(_.pick(req.body, ['first_name','last_name', 'email','password','user_role']));
-    const salt = await bcrypt.genSalt(5);
-    newUser.password = await bcrypt.hash(newUser.password, salt); 
-    newUser = await newUser.save();
-
-    // Generate auth token and send. // Instead send confimation mail and respond user to notify that
-    // const token = newUser.genAuthToken();
-    // res.header('x-auth-token', token).send(_.pick(newUser, ['_id', 'first_name','last_name', 'email']));
-    
-    // Async function to send mail.
-    jwt.sign(
-        {
-            user: _.pick(newUser, 'id'),
-        },
-        process.env.WC_jwtPrivateKey,
-        {
-            expiresIn: '1d'
-        },
-        (err, emailToken) => {
-            if(err)
-                res.status(500).send({
-                    message: "Failed to sign jwt token"
-                });
-            sendMail(newUser, emailToken);
-        }
-    );
-
-    res.status(202).send({
-        message: "Please verify your email by clicking on the link we just mailed you"
-    });
-});
-
-// Email verification route.
-router.get('/confirmation/:token', async (req, res)=>{
-    try{
-        const {user:{id}} = jwt.verify(req.params.token, process.env.WC_jwtPrivateKey);
-        await User.updateOne({_id:id}, {$set:{confirmed: true}});
-        res.redirect(`http://127.0.0.1:5000/users/${id}`);
-    } catch(error){
-        res.status(500).send({message: error});
-    }
 });
 
 // Update user profile route.
