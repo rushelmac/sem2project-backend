ff74dd1bef7733236fdd0b885fb3e6d89b9c62ea
rushelmac <rushishelke633@gmail.com>
2021-06-07 18:06:58 +0530

    Route to fetch user profile from id

--- routes/user-profile.js
+++ routes/user-profile.js
@@ -13,8 +13,14 @@ router.post('/user-profile', (req, res) => {
         res.status(200).send({message: "Profile created"});
     });
 });
+
 // Read
-router.get('/user-profile');
+router.get('/user-profile/:id', (req, res) => {
+    userProfile.findOne({"user_id":req.params.id}, (err, foundProfile) => {
+        if(err) return res.status(204).send(err);
+        res.send(foundProfile);
+    });
+});
 // Update
 router.put('/user-profile');
 // Delete
