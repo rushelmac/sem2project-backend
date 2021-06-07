be1edbc4728d29d6770dc46f503c026e849261a0
rushelmac <rushishelke633@gmail.com>
2021-06-07 18:02:27 +0530

    Route to create user profile

--- routes/user-profile.js
+++ routes/user-profile.js
@@ -1,4 +1,23 @@
 const express   = require('express'),
+router      = express.Router(),
 { userProfile } = require('../models/userProfile');
+
+
+
+// Create
+router.post('/user-profile', (req, res) => {
+    userProfile.create(req.body, (err, createdProfile) => {
+        if(err) return res.status(500).send({message: "Couldn't create user profile"});
+        createdProfile.user_id = req.user._id;
+        createdProfile.save();
+        res.status(200).send({message: "Profile created"});
+    });
+});
+// Read
+router.get('/user-profile');
+// Update
+router.put('/user-profile');
+// Delete
+router.delete('/user-profile');
 
 module.exports = router;
