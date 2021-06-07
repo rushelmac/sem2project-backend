9abf7b49a472a5a7dd849f29f51defe43834360f
rushelmac <rushishelke633@gmail.com>
2021-06-07 18:02:01 +0530

    Updated user profile schema

--- models/userProfile.js
+++ models/userProfile.js
@@ -1,38 +1,44 @@
-const { Mongoose } = require("mongoose");
+const  mongoose  = require("mongoose");
 
-const userProfile = new mongoose.schema({
+const userProfileSchema = new mongoose.Schema({
     user_id:{
-        type: Mongoose.Schema.Type.object_id
+        type: mongoose.Schema.Type.object_id
     },
     professional_info: [
         {
-            post: String,
-            company: String,
-            from:String,
-            to: String
+            post    : String,
+            company : String,
+            from    : String,
+            to      : String
         }
     ],
     educational_info:[
         {
-            school:String,
-            course:String,
-            score:String,
-            outof:String,
+            school  : String,
+            course  : String,
+            score   : String,
+            outof   : String,
         }
     ],
     personal_info: {
-        about_me: String,
-        birthdate:Date,
+        about_me    : String,
+        birthdate   : Date
     },
     contact_info: {
-        mobile_no: Number,
-        email: String,
+        mobile_no   : Number,
+        email       : String
     },
     profiles: {
-        linkedin: String,
-        github: String,
-        twitter: String,
+        linkedin    : String,
+        github      : String,
+        twitter     : String,
+        facebook    : String,
+        youtube     : String,
+        instagram   : String
     }
 });
+
+// Creating mongoDB model from defined schema
+const userProfile = mongoose.model('userProfile', userProfileSchema);
 
 module.exports.userProfile = userProfile;
