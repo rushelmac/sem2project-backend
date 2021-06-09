8759055cbb828a7f4d3e7e17ada16adac098c5c9
cognitive-ninja <saurabhhirugade@gmail.com>
2021-06-09 09:15:19 +0530

    Socket errors fixed

--- socket/index.js
+++ socket/index.js
@@ -1,6 +1,6 @@
 const io = require("socket.io")(5001, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: '*',
   },
 });
 
@@ -8,15 +8,15 @@ let users = [];
 
 const addUser = (userId, socketId) => {
   !users.some((user) => user.userId === userId) &&
-    users.push({ userId, socketId });
+  userId!==null &&  users.push({ userId, socketId });
 };
 
 const removeUser = (socketId) => {
-  users = users.filter((user) => user.socketId !== socketId);
+  users = users.filter((user) => user && user.socketId !== socketId);
 };
 
 const getUser = (userId) => {
-  return users.find((user) => user.userId === userId);
+  return users.find((user) =>user && user.userId === userId);
 };
 
 io.on("connection", (socket) => {
@@ -32,7 +32,7 @@ io.on("connection", (socket) => {
   //send and get message
   socket.on("sendMessage", ({ senderId, receiverId, text }) => {
     const user = getUser(receiverId);
-    io.to(user.socketId).emit("getMessage", {
+    user && io.to(user.socketId).emit("getMessage", {
       senderId,
       text,
     });
