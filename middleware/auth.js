8f9aaa79a575d1fd41b9c4aab0a440c254856c7e
rushelmac <rushishelke633@gmail.com>
2021-10-14 12:13:25 +0530

    Commented experimental code

--- middleware/auth.js
+++ middleware/auth.js
@@ -14,4 +14,23 @@ function auth(req, res, next) {
     }
 }
 
+// const authenticateJWT = (req, res, next) => {
+//     const authHeader = req.headers.authorization;
+
+//     if (authHeader) {
+//         const token = authHeader.split(' ')[1];
+
+//         jwt.verify(token, accessTokenSecret, (err, user) => {
+//             if (err) {
+//                 return res.sendStatus(403);
+//             }
+
+//             req.user = user;
+//             next();
+//         });
+//     } else {
+//         res.sendStatus(401);
+//     }
+// };
+
 module.exports = auth;
